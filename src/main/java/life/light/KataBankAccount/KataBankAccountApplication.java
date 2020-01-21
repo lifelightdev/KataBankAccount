2b965da95d6eea106288e20a0ad3ddac4351419f
Life Light <lifelightdev@gmail.com>
2020-01-21 17:13:37 +0100

    init

---
+++ src/main/java/life/light/KataBankAccount/KataBankAccountApplication.java
@@ -0,0 +1,13 @@
+package life.light.KataBankAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataBankAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataBankAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/life/light/KataBankAccount/KataBankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package life.light.KataBankAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataBankAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
